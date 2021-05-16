fbc18c84185f8a113d6d0d9f7c7432a2d0cdcdbb
Josiah Morris <josiahbmorris@hotmail.com>
2021-05-17 01:31:24 +1000

    Added functionality to calculator() function which allows it to pass tests

--- src/code/calculator.ts
+++ src/code/calculator.ts
@@ -1,18 +1,18 @@
 const calculator = () => {
     const add = (a: number, b: number) => {
-        
+        return a + b
     }
 
     const subtract = (a: number, b: number) => {
-        
+        return a - b        
     }
 
     const multiply = (a: number, b: number) => {
-        
+        return a * b
     }
 
     const divide = (a: number, b: number) => {
-        
+        return a / b
     }
 
     return { add, subtract, multiply, divide}
