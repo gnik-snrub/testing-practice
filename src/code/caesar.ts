6dc7dd8b0099c3336d1674bfdab85c9294200424
Josiah Morris <josiahbmorris@hotmail.com>
2021-05-18 02:54:35 +1000

    Added tests and dummy function for caesar cipher

---
+++ src/code/caesar.ts
@@ -0,0 +1,5 @@
+const caesar = (str: string, shift: number = 1) => {
+    return str
+}
+
+export default caesar
---
+++ src/tests/caesar.test.ts
@@ -0,0 +1,33 @@
+import caesar from '../code/caesar'
+
+test('Caesar: Encrypts with small shift, and no wrap required', () => {
+    expect(caesar('abcde', 1)).toBe('bcdef')
+})
+
+test('Caesar: Encrypts successfully when wrapping from z -> a', () => {
+    expect(caesar('you are a cool guy', 5)).toBe('dtz fwj f httq lzd')
+})
+
+test('Caesar: Encrypts with negative shift, and no wrap required', () => {
+    expect(caesar('vwxyz', -1)).toBe('uvwxy')
+})
+
+test('Caesar: Encrypts successfully when wrapping from a -> z', () => {
+    expect(caesar('that is a dog', -5)).toBe('ocvo dn v yjb')
+})
+
+test('Caesar: Decrypts with the default shfit value of "1" when no value is specified', () => {
+    expect(caesar('abcde')).toBe('bcdef')
+})
+
+test('Caesar: Keeps relevant characters in uppercase', () => {
+    expect(caesar('ThIs Is A sEnTeNcE', 1)).toBe('UiJt Jt B tFoUfOdF')
+})
+
+test('Caesar: Throws error when non-English characters are used', () => {
+    expect(caesar('test #7', 1)).toThrow()
+})
+
+test('Caesar: Returns empty string when empty string is supplied', () => {
+    expect(caesar('')).toBe('')
+})
