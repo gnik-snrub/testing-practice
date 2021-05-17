96ff5f929cc196d06191704e45ed45c2aa6baf78
Josiah Morris <josiahbmorris@hotmail.com>
2021-05-18 04:18:36 +1000

    Added functionality to analyze() function allowing it to pass its tests

--- src/code/analyze.ts
+++ src/code/analyze.ts
@@ -1,8 +1,8 @@
 const analyze = (numArr: number[]) => {
-    const average = () => {}
-    const min = () => {}
-    const max = () => {}
-    const length = 0
+    const average = () => { return numArr.reduce((total, curr) => total += curr) / numArr.length }
+    const min = () => { return numArr.sort((a, b) => a - b)[0] }
+    const max = () => { return numArr.sort((a, b) => b - a)[0] }
+    const length = numArr.length
 
     return {
         average: average(),
